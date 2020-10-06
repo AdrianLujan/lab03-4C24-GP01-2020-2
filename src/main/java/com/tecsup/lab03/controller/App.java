5f4b655988a7baccdacbd7362d7329c8a34b20f3
Lincoln Martinez <lincoln.martinez@tecsup.edu.pe>
2020-10-06 17:03:07 -0500

    cambios 2

--- src/main/java/com/tecsup/lab03/controller/App.java
+++ src/main/java/com/tecsup/lab03/controller/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Cambios de Adrian L." );
+        System.out.println("Cambios de Lincoln M.");
     }
 }
