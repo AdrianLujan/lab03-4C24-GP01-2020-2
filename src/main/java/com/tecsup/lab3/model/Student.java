bbd92495a3c2c0a3332f73e6b5c95ae8b72d7f39
Ivanktr <ivan.oscco@tecsup.edu.pe>
2020-10-10 19:13:31 -0500

    Cambios para el branch Oscco

---
+++ src/main/java/com/tecsup/lab3/model/Student.java
@@ -0,0 +1,11 @@
+package com.tecsup.lab3.model;
+
+public class Student {
+	
+	 public static void main( String[] args )
+	    {
+	        System.out.println( "Cambios desde el Branch" );
+
+
+	    }
+}
